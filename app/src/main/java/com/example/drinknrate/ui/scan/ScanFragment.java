f824249da17690df55527aad2baab0d7dd57adcd
DRamfeldt <DRamfeldt@Gmail.com>
2020-06-19 15:48:03 +0200

    added not fully functional scanner

--- app/src/main/java/com/example/drinknrate/ui/scan/ScanFragment.java
+++ app/src/main/java/com/example/drinknrate/ui/scan/ScanFragment.java
@@ -1,21 +1,153 @@
 package com.example.drinknrate.ui.scan;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
 import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.drinknrate.MainActivity;
 import com.example.drinknrate.R;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 public class ScanFragment extends Fragment {
 
+    private BarcodeDetector barcodeDetector;
+    private CameraSource cameraSource;
+    private SurfaceView surfaceView;
+    private TextView barcodeText;
+    private String barcodeData;
+
+
+    private static final int REQUEST_CAMERA_PERMISSION = 13;
+
+
+    public void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //surfaceView = getActivity().findViewById(R.id.surface_view);
+        //barcodeText = getActivity().findViewById(R.id.barcode_text);
+
+        surfaceView = getView().findViewById(R.id.surface_view);
+        barcodeText = getView().findViewById(R.id.barcode_text);
+
+        initDetsAndSources();
+
+    }
+
+    private void initDetsAndSources() {  // initialises detectors and sources
+
+        barcodeDetector = new BarcodeDetector.Builder(getActivity())
+                .setBarcodeFormats(Barcode.ALL_FORMATS)
+                .build();
+
+        cameraSource = new CameraSource.Builder(getActivity(), barcodeDetector)
+                .setRequestedPreviewSize(1920, 1080)
+                .setAutoFocusEnabled(true)
+                .build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                // insures that the proper permissions have benn granted.
+                try {
+                    if(ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+                        cameraSource.start(surfaceView.getHolder());
+
+                    }else{
+                        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+
+                    }
+
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcode = detections.getDetectedItems();
+                if (barcode.size() !=0){
+
+                    barcodeText.post(new Runnable(){
+
+                        @Override
+                        public void run(){
+                                barcodeData = barcode.valueAt(0).displayValue;
+                                barcodeText.setText(barcodeData);
+
+                            }
+
+                    });
+                }
+
+            }
+        });
+
+    }
+
+/*
+    @Override
+    protected void onPause(){
+        super.onPause();
+        getSupportActionBar().hide();
+        cameraSource.release();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getSupportActionBar().hide();
+        initDetsAndSources();
+    }
+ */
+
+
+
+
+
+
+
+
+/*
     private ScanViewModel scanViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -32,4 +164,7 @@ public class ScanFragment extends Fragment {
         });
         return root;
     }
+
+
+ */
 }
